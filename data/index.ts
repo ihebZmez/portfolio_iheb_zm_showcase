0f153911169d5162c88870e6e62a94266702d644
ihebZm <iheb@cfac.com.tn>
2024-07-16 19:21:46 +0100

    adding all needed icons and changing some caracters

---
+++ data/index.ts
@@ -0,0 +1,448 @@
+export const navItems = [
+  { name: "Accueil", link: "#" },
+  { name: "À Propos", link: "#about" },
+  { name: "Projets", link: "#projects" },
+  { name: "Compétences", link: "#projects" },
+  { name: "Témoignages", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title:
+      "Je Privilégie La Collaboration Et La Communication Ouverte Avec Les Clients.",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title:
+      "Je m'adapte Aisément À Divers Fuseaux Horaires Et Environnements De Travail.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-4",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 5,
+    title: "Finances & Comptabilité",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-4",
+    imgClassName: "",
+    titleClassName: "justify-end",
+    img: "/grid.svg",
+    spareImg: "/Finances_Comptabilite_resized.svg",
+  },
+  {
+    id: 3,
+    title: "Compétences & Expériences",
+    description: "Différents Outils",
+    className: "lg:col-span-3 md:col-span-3 md:row-span-3",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 7,
+    title: "Meilleur prix sur le marché de la technologie",
+    description: "",
+    className: "lg:col-span-3 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+  {
+    id: 4,
+    title: "Administration & Gestion",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/Administration_Gestion.svg",
+  },
+  {
+    id: 8,
+    title: "Informatique & Sécurité",
+    description: "",
+    className: "lg:col-span-3 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+  {
+    id: 9,
+    title: "Formuler & Développer",
+    description: "",
+    className: "lg:col-span-3 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+  {
+    id: 10,
+    title: "Meilleure Qualité De Code Pour Un Futur Évolutif",
+    description: "Meilleures Modélisations",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 11,
+    title: "Ventes & Marketing",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 12,
+    title: "Laissez-moi Vous Aider à Réaliser vos Projets",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+
+  {
+    id: 6,
+    title: "Comment me Contacter",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Application « Stocks-IT »",
+    des: "Explorez les merveilles de notre système de gestion des stocks avec cette solution captivante utilisant Angular et Spring Boot.",
+    img: "/p1.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "/ui.earth.com",
+  },
+  {
+    id: 2,
+    title: "Application « Réservatory »",
+    des: "Simplifiez votre expérience de réservation. Entraînez-vous à la salle de sport avec des collègues et des amis.",
+    img: "/p2.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "/ui.yoom.com",
+  },
+  {
+    id: 3,
+    title: "Application « Payment-Stack »",
+    des: "Une VRAIE application Software-as-a-Service avec des fonctionnalités d'IA et un système de paiements et de crédits utilisant la dernière pile technologique.",
+    img: "/p3.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "/ui.aiimg.com",
+  },
+  {
+    id: 4,
+    title: "Site Web Animé Huawei Pura 70 Ultra 3D",
+    des: "Recréation du site Web du HUAWEI Pura 70 Ultra, combinant des animations GSAP et des effets 3D Three.js.",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "/ui.apple.com",
+  },
+  {
+    id: 4,
+    title: "Site Web Animé Huawei Pura 70 Ultra 3D",
+    des: "Recréation du site Web du HUAWEI Pura 70 Ultra, combinant des animations GSAP et des effets 3D Three.js.",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "/ui.apple.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Travailler avec Iheb a été un véritable plaisir. Son professionnalisme, sa rapidité et son dévouement à fournir des résultats exceptionnels étaient évidents tout au long de notre projet. L'enthousiasme d'Adrian pour chaque aspect du développement est remarquable. Si vous souhaitez améliorer votre site web et rehausser votre marque, Iheb est le partenaire idéal.",
+    name: "Ali KANZARI",
+    title:
+      "Président de Chambre syndicale du photovoltaïque | Solar Energy Systems",
+    image_profil: "/Ali-KANZARI.png",
+  },
+  {
+    quote:
+      "Iheb est un développeur full stack fiable, réactif et doté d'un vrai sens du professionnalisme. Nous bénéficions de ses conseils et de son accompagnement en matière de développement web, d'architecture logicielle et de solutions techniques. Un accompagnement de qualité, marqué par la rigueur, la fiabilité et la bienveillance d'Iheb. Aujourd'hui, Iheb est un véritable partenaire de notre société, acteur de notre croissance.",
+    name: "Anis MSAHLI",
+    title:
+      "CEO & Founder CFAC Group | Ingénieur Financier | Commissaire Aux Comptes | Directeur des systèmes d'information | Auditeur Tierce Partie",
+    image_profil: "/Anis-MSAHLI.png",
+  },
+  {
+    quote:
+      "Sans conteste, Iheb est l'un des programmeurs les plus talentueux du marché. Lorsque je me trouve face à des défis complexes, je fais systématiquement appel à ses compétences, et il répond toujours à mes attentes. Doté d'une intelligence remarquable, d'une grande fiabilité et d'un professionnalisme exemplaire, il ne vous décevra pas.",
+    name: "Mohamed AYARI",
+    title: "AI/ML Engineer | Building real-world ML products",
+    image_profil: "/Mohamed-AYARI.png",
+  },
+  {
+    quote:
+      "Personnel jeune et dynamique, toujours ponctuel, accueillant et doté d'une grande empathie pour identifier et anticiper les difficultés, en proposant systématiquement des alternatives concrètes. - Marchetto Giovanni & Pollani Liberto, Directeur de l'usine et Avocat général d'Elettro BCA.",
+    name: "Sadok BEN YEDDER",
+    title: "CEO Groupe CAFÉ",
+    image_profil: "Sadok-BEN-YEDDER.png",
+  },
+  {
+    quote:
+      "Du début du projet jusqu'à son achèvement, Iheb nous a accompagné et a dépassé nos attentes à tous points de vue. Non seulement notre expérience a été personnelle et amicale, mais sa capacité à identifier et à présenter nos messages clés de manière imaginative et créative nous a donné une énorme confiance en lui.",
+    name: "Feres FOUDHAILI",
+    title: "Architecte De Solutions Cloud",
+    image_profil: "/Feres-FOUDHAILI.png",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "Angular",
+    img: "/Tech/angular-icon.svg",
+    nameImg: "TO DO",
+  },
+  {
+    id: 4,
+    name: "c-sharp-icon",
+    img: "/Tech/c-sharp-icon.svg",
+    nameImg: "TO DO",
+  },
+  {
+    id: 5,
+    name: "dotnet-icon",
+    img: "/Tech/dotnet-icon.svg",
+    nameImg: "TO DO",
+  },
+  {
+    id: 6,
+    name: "flutter-icon",
+    img: "/Tech/flutter-icon.svg",
+    nameImg: "TO DO",
+  },
+  {
+    id: 7,
+    name: "html-icon",
+    img: "/Tech/html-icon.svg",
+    nameImg: "TO DO",
+  },
+  {
+    id: 8,
+    name: "java-icon",
+    img: "/Tech/java-icon.svg",
+    nameImg: "TO DO",
+  },
+  {
+    id: 9,
+    name: "javascript-icon",
+    img: "/Tech/javascript-icon.svg",
+    nameImg: "TO DO",
+  },
+  {
+    id: 10,
+    name: "laravel",
+    img: "/Tech/laravel-icon.svg",
+    nameImg: "TO DO",
+  },
+  {
+    id: 11,
+    name: "mariadb-icon",
+    img: "/Tech/mariadb-icon.svg",
+    nameImg: "TO DO",
+  },
+  {
+    id: 12,
+    name: "maven-icon",
+    img: "/Tech/maven-icon.svg",
+    nameImg: "TO DO",
+  },
+  {
+    id: 13,
+    name: "mongo-icon",
+    img: "/Tech/mongo-icon.svg",
+    nameImg: "TO DO",
+  },
+  {
+    id: 14,
+    name: "mysql-icon",
+    img: "/Tech/mysql-icon.svg",
+    nameImg: "TO DO",
+  },
+  {
+    id: 15,
+    name: "nextjs-icon",
+    img: "/Tech/nextjs-icon.svg",
+    nameImg: "TO DO",
+  },
+  {
+    id: 16,
+    name: "node-js-icon",
+    img: "/Tech/node-js-icon.svg",
+    nameImg: "TO DO",
+  },
+  {
+    id: 17,
+    name: "php-icon",
+    img: "/Tech/php-icon.svg",
+    nameImg: "TO DO",
+  },
+  {
+    id: 18,
+    name: "postgresql-icon",
+    img: "/Tech/postgresql-icon.svg",
+    nameImg: "TO DO",
+  },
+  {
+    id: 19,
+    name: "redis-icon",
+    img: "/Tech/redis-icon.svg",
+    nameImg: "TO DO",
+  },
+  {
+    id: 20,
+    name: "redux-icon",
+    img: "/Tech/redux-icon.svg",
+    nameImg: "TO DO",
+  },
+  {
+    id: 21,
+    name: "ruby-icon",
+    img: "/Tech/ruby-icon.svg",
+    nameImg: "TO DO",
+  },
+  {
+    id: 22,
+    name: "spring-icon",
+    img: "/Tech/spring-icon.svg",
+    nameImg: "TO DO",
+  },
+  {
+    id: 23,
+    name: "symfony-icon",
+    img: "/Tech/symfony-icon.svg",
+    nameImg: "TO DO",
+  },
+  {
+    id: 24,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 25,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 26,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Développeur principal - CFAC",
+    desc: "Responsable sur le développement de trois applications web en utilisant PHP, React, Spring Boot en améliorant l'interactivité.",
+    tasks_desc:
+      "Développer une application web de facturation, paiements, suivi des dépenses, tâches facturables, les opérations financières, l'envoi des factures, la réception de paiements et le suivi des performances pour toutes les 11 entreprises.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Secrétaire Général - CSPV - UTICA",
+    desc: "Responsable sur le développement, le désigne et la mise à jour de site web en utilisant WordPress et Photoshop ainsi la commercialisation.",
+    tasks_desc:
+      "Aide au développement d'une plate-forme Web, transmettre les courriers et les e-mails et la responsabilité administrative et financière améliorant l'interactivité.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Projet de développement d'applications indépendant",
+    desc: "Développer un site WordPress pour une agence de communication.",
+    tasks_desc:
+      "Choisir un thème, Personnaliser le design, Intégrer du contenu, Hébergement, Google analytique, moteurs de recherche",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Projet de développement d'applications indépendant",
+    desc: "Développer un site pour la publicité, la réservation de chalets, de terrains de camping et de randonnées pour toutes les saisons de l'année au Canada.",
+    tasks_desc:
+      "Choix de la plateforme et du domaine, Conception de l'architecture, développement du site, Intégration de fonctionnalités supplémentaires, Mise en place la gestion des réservations.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 5,
+    title: "Projet de développement d'applications indépendant",
+    desc: "Développer mon site Web Next.js. Mettre en place les animations, contenu de site, les notifications par la section de contact.",
+    tasks_desc:
+      "À l'aide de Next.js, React.js, Three.js, Framer Motion, Tailwind CSS et Figma, je développe des sites web innovants et performants. En utilisant les meilleures pratiques et technologies pour offrir une expérience utilisateur exceptionnelle.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 6,
+    title: "Développeur Full Stack - Arem Group",
+    desc: "Aide au développement d'une application de gestion de stocks. Qu'elle permet de Manipuler (les articles, les bons de livraison, les expéditions...)",
+    tasks_desc:
+      "Développement d'une partie du cycle interne d'une plateforme web utilisant Spring Boot, Angular, ainsi que la modélisation UML.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
