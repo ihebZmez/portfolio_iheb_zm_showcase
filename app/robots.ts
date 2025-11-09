8901a89a0932d78e2b8a1f93c18b417f139a8808
ihebZmez <130846333+ihebZmez@users.noreply.github.com>
2025-11-09 21:43:16 +0100

    manually correct the searsh google

--- app/robots.ts
+++ app/robots.ts
@@ -2,11 +2,14 @@ import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-      disallow: ["/admin/", "/private/"],
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/admin/", "/private/"],
+        crawlDelay: 1,
+      },
+    ],
     sitemap: "https://portfolio-iheb-zm.vercel.app/sitemap.xml",
   };
 }
